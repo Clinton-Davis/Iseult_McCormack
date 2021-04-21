5a14db4cce2c3e78ce3430d663124d3fb1229448
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-04-21 20:28:27 +0100

    added a list of europe ids

---
+++ bag/countries.py
@@ -0,0 +1,23 @@
+
+europe = [
+        "AL","AD","DK","EE","FO","GL","IS",\
+        "LV","LT","NO","SE","AT","BE",\
+        "FR","DE","LI","LU","NL","CH","UA",\
+        "SK","PL","MD","HU","HR","CZ","BG",\
+        "BY","CY","GR","IT","MT","MC","ME",\
+        "PT","SM","RS","ES","TR"
+            ] 
+uk = ["UK"] 
+ire = ["IE"]
+
+zones = {
+    "Europe": 6.00,
+    "UK": 5.50,
+    "Ireland": 0.00,
+    "USA": 7.00,
+    "Canada": 700,
+    "Africa": 700,
+    "Australia" :700,
+    "New Zealand" :700,
+    "South America": 700,
+}
