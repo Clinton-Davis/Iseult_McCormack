37766b7a659c768f130c2072b6f1d0e9106f0997
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-04-29 20:35:14 +0100

    changed the order of the addresss and got rid of extra field

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -81,9 +81,10 @@ form.addEventListener("submit", function (ev) {
               address: {
                 line1: $.trim(form.street_address1.value),
                 line2: $.trim(form.street_address2.value),
+                postal_code: $.trim(form.postcode.value),
                 city: $.trim(form.town_or_city.value),
-                country: $.trim(form.country.value),
                 state: $.trim(form.county.value),
+                country: $.trim(form.country.value),
               },
             },
           },
@@ -94,9 +95,9 @@ form.addEventListener("submit", function (ev) {
               line1: $.trim(form.street_address1.value),
               line2: $.trim(form.street_address2.value),
               city: $.trim(form.town_or_city.value),
-              country: $.trim(form.country.value),
               postal_code: $.trim(form.postcode.value),
               state: $.trim(form.county.value),
+              country: $.trim(form.country.value),
             },
           },
         })
