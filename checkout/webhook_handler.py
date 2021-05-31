8960b2ff2135b73abfb86ee9f45b8dd6d622de02
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-05-31 14:12:08 +0100

    running webhook test

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -88,8 +88,8 @@ class StripeWH_Handler:
             order.stripe_receipt = stripe_receipt
             order.save()
             
-            self._shopping_confirmation_email(order)
-            self._email_order(order)
+            # self._shopping_confirmation_email(order)
+            # self._email_order(order)
             return HttpResponse(
                 content=f'Webhook received: {event["type"]} | SUCCESS: Verified in database',
                 status=200)
