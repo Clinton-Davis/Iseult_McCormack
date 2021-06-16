f8232eea1dab20064d80d41bc6946780349c664e
Clinton Davis <53115265+Clinton-Davis@users.noreply.github.com>
2021-06-16 16:51:26 +0100

    added test to bag and checkout

--- checkout/tests/tests_view.py
+++ checkout/tests/tests_view.py
@@ -1,12 +1,23 @@
-from django.test import TestCase
-from profiles.models import UserProfile
-from django.contrib.auth.models import User
+from django.test import TestCase, Client
+from django.urls import reverse
 
 
+class TestCheckoutViews(TestCase):
 
+    def setUp(self):
+        self.client = Client()
+        self.checkout = reverse("checkout:checkout_payment")
 
-    
-    
-    
+    def test_checkout_payment_view(self):
+        response = self.client.get(self.checkout)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('checkout/checkout_payment.html')
 
+    def setUp(self):
+        self.client = Client()
+        self.checkout = reverse("checkout:address")
 
+    def test_checkout_address_view(self):
+        response = self.client.get(self.checkout)
+        self.assertEqual(response.status_code, 302)
+        self.assertTemplateUsed('checkout/checkout_address.html')
